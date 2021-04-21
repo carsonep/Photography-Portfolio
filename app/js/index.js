dbc71119cc6e40ad0c8dc0286bcbd16ed2a4e0bd
carsonep <carsonepeterson@gmail.com>
2021-04-21 18:56:33 -0500

    touched up responsiveness + new features

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,22 @@
 let btn = document.getElementsByClassName("btn");
 let slide = document.getElementById("slide");
 
+const mainMenu = document.querySelector(".nav__list");
+const closeMenu = document.querySelector(".closeMenu");
+const openMenu = document.querySelector(".openMenu");
+
+openMenu.addEventListener("click", show);
+closeMenu.addEventListener("click", close);
+
+function show() {
+  mainMenu.style.display = "flex";
+  mainMenu.style.top = "0";
+}
+
+function close() {
+  mainMenu.style.top = "-100%";
+}
+
 btn[0].onclick = function () {
   slide.style.transform = "translatex(0px)";
   for (i = 0; i < 4; i++) {
@@ -29,3 +45,38 @@ btn[3].onclick = function () {
   }
   this.classList.add("active");
 };
+
+let form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+      Accept: "application/json",
+    },
+  })
+    .then((response) => {
+      status.innerHTML = "Success! Please Allow 24-48 hours for a response!";
+      status.classList.add("success");
+      form.reset();
+    })
+    .catch((error) => {
+      status.innerHTML = "Oops! There was a problem submitting your form";
+      status.classList.add("error");
+    });
+}
+form.addEventListener("submit", handleSubmit);
+
+gsap.registerPlugin(ScrollTrigger);
+
+ScrollTrigger.batch("#card", {
+  interval: 0.1, // time window (in seconds) for batching to occur.
+  batchMax: 3, // maximum batch size (targets)
+  onEnter: (batch) => gsap.to(batch, { autoAlpha: 1, stagger: 0.1 }),
+  // also onLeave, onEnterBack, and onLeaveBack
+  // also most normal ScrollTrigger values like start, end, etc.
+});
